348ad06221780f838231ab416935460329c4cb6d
Urbi Ghosh <55971386+UrbiGhosh@users.noreply.github.com>
2022-07-28 18:44:06 +0530

    Create toll_gate.cpp

---
+++ other/toll_gate.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+int n;
+int cost[25];
+int mens[25];
+
+int ans;
+
+void solve(int index, int one, int two, int three, int total){
+    
+    // optimization
+    if(total > ans)return;
+    
+    int rented = one + two + three;
+    
+    // base case
+    if(index == n-1){
+        if(rented >= mens[n-1])
+            ans = min(ans, total);
+        else 
+            ans = min(ans, total + cost[n-1]);
+        return;
+    }
+    
+    // recursive case
+
+    // hire the men
+    solve(index + 1, one + mens[index], two, three, total + 2*cost[index]);
+    
+    //pay the cost and move on 
+    solve(index + 1, one, two, three, total + cost[index]);
+    
+    // fight the men 
+
+    if(rented >= mens[index]){
+        if(two + three < mens[index])
+            one = rented - mens[index];
+        if(three < mens[index])
+            two = (two + three <= mens[index]) ? 0 : two - mens[index] + three;
+        
+        solve(index + 1, 0, one, two, total);
+    }
+    
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(0);
+
+    cin>>n;
+   
+    for(int i = 0; i<n; i++)
+        cin>>mens[i]>>cost[i];
+    
+    ans = 1000000;
+    
+    solve(0,0,0,0,0);
+    cout<<ans<<endl;
+    return 0;
+}
