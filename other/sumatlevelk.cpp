e2797f1ef84cf1f846f1433b620866bf185f3b36
Urbi Ghosh <55971386+UrbiGhosh@users.noreply.github.com>
2022-07-31 11:43:58 +0530

    Update sumatlevelk.cpp

--- other/sumatlevelk.cpp
+++ other/sumatlevelk.cpp
@@ -41,3 +41,66 @@ int main(){
     }
     return 0;
 }
+
+
+
+
+//////////////////////
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int k;
+    cin>>k;
+    string s;
+    cin>>s;
+    int open=0;
+    int close=0;
+    vector<pair<int,int>> store;
+    int prev=0;
+    for(int i=0;i< s.size();i++){
+        if(s[i]=='(')open++;
+        else if(s[i]==')')close++;
+        else{
+            int val=s[i]-'0';
+            if (prev!=0){
+                val = prev*10 + val;
+                if(s[i+1]!='(' && s[i+1]!=')'){
+                    prev=val;
+                }
+                else {
+                    prev=0;
+                    store.push_back({val,open-close});
+                }
+            }
+            
+            else if(s[i+1]!='(' && s[i+1]!=')'){
+                prev=val;
+            
+            }
+            else {
+                prev=0;
+                store.push_back({val,open-close});
+                
+            }
+                
+        }
+    }
+    int ans=0;
+    for(int i=0;i<store.size();i++){
+      //  cout<<store[i].first<<" "<<store[i].second<<"\n";
+        if(store[i].second==k+1)
+            ans+=store[i].first;
+    }
+    cout<<ans;
+    return 0;
+}
+
